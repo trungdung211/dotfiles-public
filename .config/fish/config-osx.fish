078d6d8dd546ceb6186913c53ecb8f852b91b5de
Hong Hieu <hanajp97@gmail.com>
2022-05-24 22:35:21 +0700

    update fish

--- .config/fish/config-osx.fish
+++ .config/fish/config-osx.fish
@@ -1,4 +1,6 @@
 if type -q exa
   alias ll "exa -l -g --icons"
+  alias lv "exa -l -g --icons -a --tree --level"
   alias lla "ll -a"
+  alias ide "tmux split-window -v -p 30 && tmux split-window -h -p 50"
 end
